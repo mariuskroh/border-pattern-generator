d2a9a7774fdbe41184f00d0de30025b57e513bcd
Marius Kroh <mail@mariuskroh.de>
2020-02-26 18:46:59 +0100

    :art: Structure folders for setting up webpack

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const screen = document.querySelector("#canvas");
+const base = 16;
+const borderWidth = 4;
+
+const createPattern = document.querySelector(".create-pattern");
+
+function clearCanvas() {
+  screen.innerHTML = "";
+}
+
+function makePattern() {
+  clearCanvas();
+  let int = base;
+  for (let i = 0; i < int * int; i++) {
+    const gridElement = document.createElement("div");
+    addBorder(gridElement);
+    gridElement.classList.add("grid-element");
+    screen.appendChild(gridElement);
+  }
+}
+
+function addBorder(item) {
+  const randomNumber = Math.floor(Math.random() * Math.floor(3));
+  const borderVariations = [
+    `${borderWidth}px ${borderWidth}px ${borderWidth}px 0`,
+    `${borderWidth}px ${borderWidth}px 0 ${borderWidth}px`,
+    `${borderWidth}px 0 ${borderWidth}px ${borderWidth}px`,
+    `0 ${borderWidth}px ${borderWidth}px ${borderWidth}px`
+  ];
+  item.style.borderWidth = borderVariations[randomNumber];
+  return item;
+}
+
+makePattern();
+
+createPattern.addEventListener("click", makePattern);
