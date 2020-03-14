11486ad07c365fea23c53700e3299bf791631f9d
Marius Kroh <mail@mariuskroh.de>
2020-03-14 10:25:22 +0100

    :tada: Init

---
+++ dev/js/globals.js
@@ -0,0 +1,9 @@
+//globals
+let globals = {
+  base: 16,
+  borderWidth: 4,
+  screen: document.querySelector("#canvas"),
+  createPattern: document.querySelector(".create-pattern")
+};
+
+module.exports = globals;
