412ecf59e30b14ecba825d4f4f7095389343345c
Marius Kroh <mail@mariuskroh.de>
2020-03-16 13:23:45 +0100

    :sparkles: Create grid elements function

--- src/js/create-grid-elements.js
+++ src/js/create-grid-elements.js
@@ -1,29 +1,13 @@
-import { base, borderWidth, canvas } from "./globals";
-
+import { base, canvas } from "./globals";
+import getGridElementStyle from "./get-grid-element-style.js";
 //all values from globals should come from "get settings" later
 const createGridElements = function() {
-  let int = base;
-  for (let i = 0; i < int * int; i++) {
+  for (let i = 0; i < base * base; i++) {
     const gridElement = document.createElement("div");
-    gridElement.style.width = "20px";
-    gridElement.style.height = "20px";
-    addBorder(gridElement);
+    getGridElementStyle(gridElement);
     gridElement.classList.add("grid-element");
     canvas.appendChild(gridElement);
   }
 };
 
 export default createGridElements;
-
-function addBorder(item) {
-  const randomNumber = Math.floor(Math.random() * Math.floor(5));
-  const borderVariations = [
-    `${borderWidth}px ${borderWidth}px ${borderWidth}px 0`,
-    `${borderWidth}px ${borderWidth}px 0 ${borderWidth}px`,
-    `${borderWidth}px 0 ${borderWidth}px ${borderWidth}px`,
-    `0 ${borderWidth}px ${borderWidth}px ${borderWidth}px`,
-    `0`
-  ];
-  item.style.borderWidth = borderVariations[randomNumber];
-  return item;
-}
