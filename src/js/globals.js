e079f281c1509689e92d8093b4552af764be6715
Marius Kroh <mail@mariuskroh.de>
2020-03-16 19:36:27 +0100

    :fire: Remove unused vars

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,8 +1,3 @@
-//startup settings
-export let base = 24;
-export let gapSize = 16;
-export let borderWidth = 1;
-
 //dom elements
 export const canvas = document.querySelector("#canvas");
 export const createPattern = document.querySelector(".create-pattern");
