d5687f20cc8a373dee6728babb21d396b2944b33
Marius Kroh <mail@mariuskroh.de>
2020-03-16 12:33:01 +0100

    :ambulance: Fix create-grid calculation

--- src/js/create-grid.js
+++ src/js/create-grid.js
@@ -2,10 +2,8 @@ import { base, gapSize, canvas } from "./globals";
 
 //all values from globals should come from "get settings" later
 const createGrid = function() {
-  const canvasWidth = canvas.clientWidth;
-  console.log(canvasWidth);
-  canvas.style.gridTemplateColumns = `repeat(${base}, ${canvasWidth / base}px)`;
-  canvas.style.gridTemplateRows = `repeat(${base}, ${canvasWidth / base}px)`;
+  canvas.style.gridTemplateColumns = `repeat(${base}, 1fr`;
+  canvas.style.gridTemplateRows = `repeat(${base}, 1fr)`;
   canvas.style.gridColumnGap = `${gapSize}px`;
   canvas.style.gridRowGap = `${gapSize}px`;
 };
