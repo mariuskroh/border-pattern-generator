bbe3d4c10e101d15e1188b187c925b0209097b54
Marius Kroh <mail@mariuskroh.de>
2020-03-16 18:46:13 +0100

    :tada: Init settings object

--- src/js/get-settings.js
+++ src/js/get-settings.js
@@ -1,5 +1,14 @@
-const sliderBaseCount = document.querySelector("#base-count");
-
-console.log(sliderBaseCount);
-
-module.exports = sliderBaseCount;
+const baseCount = document.querySelector("#base-count");
+const borderWidth = document.querySelector("#border-width");
+const gapSize = document.querySelector("#gap-size");
+export let settings = {};
+export const getSettings = function() {
+  settings = {
+    baseCount: baseCount.value,
+    borderWidth: borderWidth.value,
+    gapSize: gapSize.value
+  };
+  console.clear();
+  console.table(settings);
+  return settings;
+};
