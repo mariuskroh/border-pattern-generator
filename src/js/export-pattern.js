02b261fe0171d25307f5f55dcc8cfb4ac723debd
Marius Kroh <mail@mariuskroh.de>
2020-03-18 22:56:16 +0100

    :sparkles: : Add export as .png + autodownload

--- src/js/export-pattern.js
+++ src/js/export-pattern.js
@@ -1,9 +1,25 @@
 import { canvas } from "./globals.js";
 import html2canvas from "html2canvas";
 
+// const options = {
+//   width: 1500,
+//   height: 1500
+// };
 export const htmlCanvas = function() {
   html2canvas(canvas).then(function(canvas) {
-    // document.body.appendChild(canvas);
+    const img = canvas.toDataURL("image/png");
+    downloadImage(img, "pattern.png");
   });
 };
 export default htmlCanvas;
+
+//Creates (and removes) dummy anchor tag for automatic download
+//Obviously not an ideal solution...
+function downloadImage(data, filename = "pattern.png") {
+  const a = document.createElement("a");
+  a.href = data;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
