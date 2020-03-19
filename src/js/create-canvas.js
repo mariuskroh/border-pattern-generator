8c38dd43aa0e19e02752efb2cd8056f36aa0444d
Marius Kroh <mail@mariuskroh.de>
2020-03-19 16:14:14 +0100

    :art: Center canvas

--- src/js/create-canvas.js
+++ src/js/create-canvas.js
@@ -1,8 +1,13 @@
 import { canvas } from "./globals";
 const createCanvas = function() {
   const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const size = canvas.offsetWidth / 2;
   canvas.style.width = `${windowWidth / 2}px`;
   canvas.style.height = `${windowWidth / 2}px`;
+  canvas.style.left = `50%`;
+  canvas.style.top = `50%`;
+  canvas.style.transform = `translate(-50%, -50%)`;
 };
 
 export default createCanvas;
