a49374f024a79ae86b1eed06a4679e28e261f104
Marius Kroh <mail@mariuskroh.de>
2020-03-19 17:22:27 +0100

    :tada: Init element style select

--- src/js/get-settings.js
+++ src/js/get-settings.js
@@ -1,12 +1,14 @@
 const baseCount = document.querySelector("#base-count");
 const borderWidth = document.querySelector("#border-width");
 const gapSize = document.querySelector("#gap-size");
+const elementStyle = document.querySelector("#element-style");
 export let settings = {};
 export const getSettings = function() {
   settings = {
     baseCount: baseCount.value,
     borderWidth: borderWidth.value,
-    gapSize: gapSize.value
+    gapSize: gapSize.value,
+    elementStyl: elementStyle.value
   };
   console.clear();
   console.table(settings);
