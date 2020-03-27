d71923a6418becf76a042d67658b44840b544c52
Marius Kroh <mail@mariuskroh.de>
2020-03-27 18:48:27 +0100

    :pencil2: Change some namings

--- src/js/get-settings.js
+++ src/js/get-settings.js
@@ -1,14 +1,14 @@
 const baseCount = document.querySelector("#base-count");
-const borderWidth = document.querySelector("#border-width");
+const strokeWidth = document.querySelector("#stroke-width");
 const gapSize = document.querySelector("#gap-size");
 const elementStyle = document.querySelector("#element-style");
 export let settings = {};
 export const getSettings = function() {
   settings = {
     baseCount: baseCount.value,
-    borderWidth: borderWidth.value,
+    strokeWidth: strokeWidth.value,
     gapSize: gapSize.value,
-    elementStyl: elementStyle.value
+    elementStyle: elementStyle.value
   };
   console.clear();
   console.table(settings);
