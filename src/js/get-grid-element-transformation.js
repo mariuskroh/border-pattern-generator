f3fe0fec268219092c5683340def0150dbd19076
Marius Kroh <mail@mariuskroh.de>
2020-03-29 17:50:04 +0200

    :zap: Add the randomize functionality

--- src/js/get-grid-element-transformation.js
+++ src/js/get-grid-element-transformation.js
@@ -1,14 +1,23 @@
 import { settings } from "./get-settings.js";
+import randomizeInput from "./randomize-input.js";
 
-const getGridElementTransformation = function(item) {
+export default function getGridElementTransformation(item) {
   const rotation = settings.rotation;
   const scale = settings.scale / 10;
   const randomize = settings.randomize;
   const randomizeRotation = settings.randomizeRotation;
-  const randomizeScale = settings.randomizeRotation;
-  // apply rotation & scale
-  item.style.transform = `rotate(${rotation}deg) scale(${scale})`;
-  return item;
-};
+  const randomizeScale = settings.randomizeScale;
+  let calculatedRotation;
+  let calculatedScale;
 
-export default getGridElementTransformation;
+  randomizeRotation
+    ? (calculatedRotation = randomizeInput(randomize, rotation))
+    : (calculatedRotation = rotation);
+
+  randomizeScale
+    ? (calculatedScale = randomizeInput(randomize, scale))
+    : (calculatedScale = scale);
+
+  item.style.transform = `rotate(${calculatedRotation}deg) scale(${calculatedScale})`;
+  return item;
+}
