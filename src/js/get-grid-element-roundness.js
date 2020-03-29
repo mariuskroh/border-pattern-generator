dcafb142d56cc903580fbd20dd6a6969ddcfb79c
Marius Kroh <mail@mariuskroh.de>
2020-03-29 17:13:59 +0200

    :zap: Enable randomize functionality

--- src/js/get-grid-element-roundness.js
+++ src/js/get-grid-element-roundness.js
@@ -1,8 +1,17 @@
 import { settings } from "./get-settings.js";
+import randomizeInput from "./randomize-input.js";
 
 const getGridElementRoundness = function(item) {
+  const randomize = settings.randomize;
   const roundness = settings.roundness;
-  item.style.borderRadius = `${roundness}%`;
+  const randomizeRoundness = settings.randomizeRoundness;
+
+  if (randomizeRoundness) {
+    item.style.borderRadius = `${randomizeInput(randomize, roundness)}%`;
+  } else {
+    item.style.borderRadius = `${roundness}%`;
+  }
+
   return item;
 };
 
