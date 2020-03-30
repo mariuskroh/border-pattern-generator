cf423fb6b05bfb23c037c6567a76a612e1f99459
Marius Kroh <mail@mariuskroh.de>
2020-03-30 21:53:35 +0200

    :fish: : Try new approach for randomization

--- src/js/randomize-input.js
+++ src/js/randomize-input.js
@@ -1,8 +1,18 @@
-export default function randomizeInput(factor, limit) {
-  //think about this later, good for now
-  // min needs to be 1 as default not zero
-  let min = limit / factor;
-  let max = limit;
-  const value = Math.floor(Math.random() * (max - min + 1) + min);
+export default function randomizeInput(rand, property) {
+  // think about this later, good for now
+
+  // approach 1
+  // let min = property / rand;
+  // let max = property;
+  // const value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  // approach 2 - let's see if this is more interesting...
+  let possibleValues = [property];
+  for (let i = 0; i < rand; i++) {
+    possibleValues.push(0);
+  }
+  const getRandomNumber = Math.floor(Math.random() * Math.floor(rand));
+  let value = possibleValues[getRandomNumber];
+
   return value;
 }
