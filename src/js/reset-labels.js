6d0fcca93ace9f4ac1913e02cd01782ce9a827b3
Marius Kroh <mail@mariuskroh.de>
2020-03-30 21:53:15 +0200

    :ambulance: Fix typo

--- src/js/reset-labels.js
+++ src/js/reset-labels.js
@@ -2,7 +2,7 @@ import displaySliderValues from "./display-slider-values.js";
 
 export default function resetLabels() {
   this.removeEventListener("mousemove", displaySliderValues);
-  this.removeEventListener("mouseup", resetLabels);
+  this.removeEventListener("mouseout", resetLabels);
   const sliderName = this.name;
   const sliderId = this.id;
   const label = document.querySelector(`label[for="${sliderId}"]`);
